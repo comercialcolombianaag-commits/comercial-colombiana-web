987196d5533bd75f704bef51de8c7181ba6a170f
comercialcolombianaag-commits <comercialcolombianaag@gmail.com>
2026-02-20 16:28:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import RiskMatrix from './components/RiskMatrix';
+import StatsSection from './components/StatsSection';
 import Features from './components/Features';
 import Services from './components/Services';
 import Testimonials from './components/Testimonials';
