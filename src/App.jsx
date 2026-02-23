bb5e3e1f9c56440de740dd79bb6db1353600307e
comercialcolombianaag-commits <comercialcolombianaag@gmail.com>
2026-02-23 16:09:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import QuienesSomos from './pages/QuienesSomos';
+import SegurosPersonas from './pages/SegurosPersonas';
 import './styles.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/inicio" element={<Home />} />
           <Route path="/quienes-somos" element={<QuienesSomos />} />
+          <Route path="/seguros-para-personas" element={<SegurosPersonas />} />
         </Routes>
         <Footer />
       </div>
