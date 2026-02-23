98b528df8cd20d04af9e58b18d210f13637a7da1
comercialcolombianaag-commits <comercialcolombianaag@gmail.com>
2026-02-23 15:36:14 -0500

    Update QuienesSomos.jsx

--- src/pages/QuienesSomos.jsx
+++ src/pages/QuienesSomos.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import AboutUs from '../components/AboutUs';
 
+const QuienesSomos = () => {
+  return (
+    <div style={{ paddingTop: '80px' }}>
+      <AboutUs />
+    </div>
+  );
+};
+
+export default QuienesSomos;
