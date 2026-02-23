a9240118490ffc07d7d417af9ca9bea6da34cf34
comercialcolombianaag-commits <comercialcolombianaag@gmail.com>
2026-02-23 15:35:29 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,32 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import ServicesGrid from '../components/ServicesGrid';
+import RiskMatrix from '../components/RiskMatrix';
+import StatsSection from '../components/StatsSection';
+import WhyChooseUs from '../components/WhyChooseUs';
+import ResultsSection from '../components/ResultsSection';
+import Testimonials from '../components/Testimonials';
+import AboutUs from '../components/AboutUs';
+import WhyUs from '../components/WhyUs';
+import InsurersSlider from '../components/InsurersSlider';
+import ContactForm from '../components/ContactForm';
 
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ServicesGrid />
+      <RiskMatrix />
+      <StatsSection />
+      <WhyChooseUs />
+      <ResultsSection />
+      <Testimonials />
+      <AboutUs />
+      <WhyUs />
+      <InsurersSlider />
+      <ContactForm />
+    </>
+  );
+};
+
+export default Home;
