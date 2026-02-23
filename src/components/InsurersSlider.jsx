6e60b63c071800242c513ad192d5e00c644bfa6d
comercialcolombianaag-commits <comercialcolombianaag@gmail.com>
2026-02-23 15:45:05 -0500

    Update InsurersSlider.jsx

--- src/components/InsurersSlider.jsx
+++ src/components/InsurersSlider.jsx
@@ -24,8 +24,8 @@ const InsurersSlider = () => {
     'Colmena Seguros',
     'Seguros Aurora',
     'Colsanitas Seguros',
-    'Cesce'
-    'Assist Card'
+    'Cesce',
+    'Assist Card',
     ];
 
   return (
